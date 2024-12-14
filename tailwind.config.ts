ca1daba50aff28c662763331db62ab51792d2e31
Blkhurst <github@blkhurst.co.uk>
2024-12-14 11:27:07 +0000

    chore: add css variables and configure tailwind to support

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,17 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "hsl(var(--background))",
+        border: "hsl(var(--border))",
+        copy: {
+          primary: "hsl(var(--copy-primary))",
+          secondary: "hsl(var(--copy-secondary))",
+          tertiary: "hsl(var(--copy-tertiary))",
+          footer: "hsl(var(--copy-footer))",
+        },
+      },
+      fontFamily: {
+        inter: ["var(--font-inter)"],
       },
     },
   },
