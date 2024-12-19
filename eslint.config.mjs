f6780fb368097f4e967d6320616f135cb98e3ec6
Blkhurst <github@blkhurst.co.uk>
2024-12-19 14:34:16 +0000

    chore(eslint): ignore __generated__ directory in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    ignores: ["**/__generated__/*"],
+  },
 ];
 
 export default eslintConfig;
