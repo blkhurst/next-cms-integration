177f8ecc3505db42335582b2ba3d6364a3476cc4
Blkhurst <github@blkhurst.co.uk>
2024-12-20 19:26:14 +0000

    feat: 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <main className="flex h-[50svh] flex-col items-center justify-center text-center">
+      <h1 className="text-4xl font-bold">404</h1>
+      <p className="mt-2 text-balance text-lg text-copy-secondary">
+        Sorry, the page you’re looking for doesn’t exist.
+      </p>
+    </main>
+  );
+}
